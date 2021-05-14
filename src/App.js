825566a7fa60797c3537f9d2b7e2c639f477be05
ranjitm10 <64469138+ranjitm10@users.noreply.github.com>
2021-05-14 22:15:22 +0530

    its should work

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b3124371`;
+		const url = `http://www.omdbapi.com/?s=batman&apikey=b3124371`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
